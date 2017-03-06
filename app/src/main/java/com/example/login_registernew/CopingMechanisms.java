e8696d7e67142017493001427a1f91c7d7059348
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-06 20:42:25 +0000

    created activity, added text to temporary textview

--- app/src/main/java/com/example/login_registernew/CopingMechanisms.java
+++ app/src/main/java/com/example/login_registernew/CopingMechanisms.java
@@ -2,6 +2,7 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CopingMechanisms extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class CopingMechanisms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coping_mechanisms);
+
+        TextView myText = (TextView) findViewById(R.id.textView);
+        myText.setText("Coping Mechanisms");
     }
 }
