b4a948386464a110413fb0b0f201bf16cd6b4c85
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-09 20:29:38 +0000

    created pop up windows for all coping mechanisms

--- app/src/main/java/com/example/login_registernew/getOrganisedPopUp.java
+++ app/src/main/java/com/example/login_registernew/getOrganisedPopUp.java
@@ -2,6 +2,8 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 public class getOrganisedPopUp extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class getOrganisedPopUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_organised_pop_up);
+
+        //get the resolution of the device screen and store it in variable dm
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        //initialize and setwidth and height variables of pop up window
+        int width = (dm.widthPixels);
+        int height = (dm.heightPixels);
+
+        //set the size of the pop up windows to 80% of the screen size
+        getWindow().setLayout((int)(width*0.8), (int)(height*0.8));
+
+        TextView mytext = (TextView) findViewById(R.id.textView);
+        mytext.setText("get organised page");
+
     }
 }
