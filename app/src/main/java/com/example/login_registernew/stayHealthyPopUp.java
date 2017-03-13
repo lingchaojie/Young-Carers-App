ef7a0ade0a61152df8da50b2332bda2dd3249132
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-13 16:05:41 +0000

    dimmed background activity

--- app/src/main/java/com/example/login_registernew/stayHealthyPopUp.java
+++ app/src/main/java/com/example/login_registernew/stayHealthyPopUp.java
@@ -3,6 +3,7 @@ package com.example.login_registernew;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class stayHealthyPopUp extends AppCompatActivity {
@@ -21,10 +22,15 @@ public class stayHealthyPopUp extends AppCompatActivity {
         int height = (dm.heightPixels);
 
         //set the size of the pop up windows to 80% of the screen size
-        getWindow().setLayout((int)(width*0.8), (int)(height*0.8));
+        getWindow().setLayout((int)(width*0.85), (int)(height*0.85));
+
+        //set background to dim
+        WindowManager.LayoutParams windowManager = getWindow().getAttributes();
+        windowManager.dimAmount = 0.5f;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
         TextView mytext = (TextView) findViewById(R.id.textView);
-        mytext.setText("stay healthy page");
+        mytext.setText("Stay Healthy");
 
     }
 }
