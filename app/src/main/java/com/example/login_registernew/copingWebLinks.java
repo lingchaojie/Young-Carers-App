c48e2bafe1c3fd938cf27fc30c27d3575f6c2283
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-13 13:35:00 +0000

    added pop up functionality

--- app/src/main/java/com/example/login_registernew/copingWebLinks.java
+++ app/src/main/java/com/example/login_registernew/copingWebLinks.java
@@ -2,6 +2,8 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 public class copingWebLinks extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class copingWebLinks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coping_web_links);
+
+        //get the resolution of the device screen and store it in variable dm
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        //initialize and setwidth and height variables of pop up window
+        int width = (dm.widthPixels);
+        int height = (dm.heightPixels);
+
+        //set the size of the pop up windows to 80% of the screen size
+        getWindow().setLayout((int)(width*0.8), (int)(height*0.8));
+
+        TextView mytext = (TextView) findViewById(R.id.textView);
+        mytext.setText("relevant web links");
     }
 }
