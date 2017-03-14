21caaea743ec8991080472d0f4f75a90d1ad6b17
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-14 23:40:40 +0000

    added on click listeners and created activities for all recipes

---
+++ app/src/main/java/com/example/login_registernew/curryRecipe.java
@@ -0,0 +1,13 @@
+package com.example.login_registernew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class curryRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curry_recipe);
+    }
+}
