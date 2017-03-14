c13c2419e78ecb25f72cbd3e1fea27c9f235d7fd
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-14 16:09:28 +0000

    added on click listeners for home and info button, created new info activity

---
+++ app/src/main/java/com/example/login_registernew/RecipesInformation.java
@@ -0,0 +1,13 @@
+package com.example.login_registernew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipesInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes_information);
+    }
+}
