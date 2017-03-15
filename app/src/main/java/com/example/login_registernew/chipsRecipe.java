6fb1bc85d32c1669097b888dcc96c9ef76a2c563
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-15 00:21:13 +0000

    added pop up window

--- app/src/main/java/com/example/login_registernew/chipsRecipe.java
+++ app/src/main/java/com/example/login_registernew/chipsRecipe.java
@@ -2,6 +2,9 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class chipsRecipe extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class chipsRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chips_recipe);
+
+        //get the resolution of the device screen and store it in variable dm
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        //initialize and setwidth and height variables of pop up window
+        int width = (dm.widthPixels);
+        int height = (dm.heightPixels);
+
+        //set the size of the pop up windows to 80% of the screen size
+        getWindow().setLayout((int)(width*0.85), (int)(height*0.85));
+
+        //set background to dim
+        WindowManager.LayoutParams windowManager = getWindow().getAttributes();
+        windowManager.dimAmount = 0.5f;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+
+        TextView mytext = (TextView) findViewById(R.id.textView);
+        mytext.setText("Homemade Chips");
     }
 }
