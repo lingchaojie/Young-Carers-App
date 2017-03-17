4aa35562713fd988c3bd78aebedc6162400c60c8
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-17 13:52:23 +0000

    initialised textviews, imagebuttons and button

--- app/src/main/java/com/example/login_registernew/carersCentre.java
+++ app/src/main/java/com/example/login_registernew/carersCentre.java
@@ -2,6 +2,11 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class carersCentre extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class carersCentre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carers_centre);
+
+        //initialise text views
+        TextView Services = (TextView) findViewById(R.id.services);
+        TextView Definition = (TextView) findViewById(R.id.definition);
+        TextView Events = (TextView) findViewById(R.id.events);
+
+        //initialise imagebuttons
+        ImageButton homeButton = (ImageButton) findViewById(R.id.homeButton);
+        ImageButton contactDetailsButton = (ImageButton) findViewById(R.id.contactDetailsButton);
+
+        //initialise feedback button
+        Button feedbackButton = (Button) findViewById(R.id.feedback);
     }
 }
