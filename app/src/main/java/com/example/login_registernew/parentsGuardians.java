03b9f52ce04bbea8d377bf9fcd40e49a4c5542f3
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-21 13:30:11 +0000

    initalised textviews

--- app/src/main/java/com/example/login_registernew/parentsGuardians.java
+++ app/src/main/java/com/example/login_registernew/parentsGuardians.java
@@ -2,6 +2,7 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class parentsGuardians extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class parentsGuardians extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parents_guardians);
+
+        //initialise text views
+        TextView ToDo = (TextView) findViewById(R.id.todo);
+        TextView supportCentre = (TextView) findViewById(R.id.supportCentre);
+        TextView supportCarer = (TextView) findViewById(R.id.supportCarer);
+
+        
     }
 }
