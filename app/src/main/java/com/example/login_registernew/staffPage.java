5585f043516f9113e64a23853d113c746a4b95da
lucywalsh <lucyhannahwalsh@gmail.com>
2017-03-27 12:46:45 +0100

    added imagebutton and on click listener to home page

--- app/src/main/java/com/example/login_registernew/staffPage.java
+++ app/src/main/java/com/example/login_registernew/staffPage.java
@@ -1,7 +1,10 @@
 package com.example.login_registernew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class staffPage extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class staffPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_page);
+
+        //imagebutton for home button
+        final ImageButton homeButton = (ImageButton) findViewById(R.id.homeButton);
+
+        //onclick listener for home button
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(staffPage.this, UserArea.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
