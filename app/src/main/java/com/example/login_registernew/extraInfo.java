58b1d0b1bf74644d8d81ec3e767cb6bbeaf73ee4
lucywalsh <lucyhannahwalsh@gmail.com>
2017-04-12 22:38:35 +0100

    initialised text views

--- app/src/main/java/com/example/login_registernew/extraInfo.java
+++ app/src/main/java/com/example/login_registernew/extraInfo.java
@@ -2,6 +2,7 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class extraInfo extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class extraInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extra_info);
+
+        //initialise text views
+        TextView freeStuff = (TextView) findViewById(R.id.freeStuff);
+        TextView places = (TextView) findViewById(R.id.places);
+        TextView carersCentreButton = (TextView) findViewById(R.id.carersCentreButton);
+
+        
     }
 }
