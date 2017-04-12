26b9702871c274c48a2d5187b097ce875988147e
lucywalsh <lucyhannahwalsh@gmail.com>
2017-04-12 22:39:48 +0100

    made textviews scrollable

--- app/src/main/java/com/example/login_registernew/extraInfo.java
+++ app/src/main/java/com/example/login_registernew/extraInfo.java
@@ -2,6 +2,7 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 public class extraInfo extends AppCompatActivity {
@@ -14,8 +15,15 @@ public class extraInfo extends AppCompatActivity {
         //initialise text views
         TextView freeStuff = (TextView) findViewById(R.id.freeStuff);
         TextView places = (TextView) findViewById(R.id.places);
+
+        //initalise button
         TextView carersCentreButton = (TextView) findViewById(R.id.carersCentreButton);
 
+        //make textviews scrollable
+        freeStuff.setMovementMethod(new ScrollingMovementMethod());
+        places.setMovementMethod(new ScrollingMovementMethod());
+
         
+
     }
 }
