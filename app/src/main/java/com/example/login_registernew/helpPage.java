5191e271e3d918308dd3be102b9b87527ee6e267
lucywalsh <lucyhannahwalsh@gmail.com>
2017-04-12 23:09:04 +0100

    initialised text views

--- app/src/main/java/com/example/login_registernew/helpPage.java
+++ app/src/main/java/com/example/login_registernew/helpPage.java
@@ -2,6 +2,7 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class helpPage extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class helpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_page);
+
+        //initialise text views
+        TextView Q1 = (TextView) findViewById(R.id.Q1);
+        TextView Q1A = (TextView) findViewById(R.id.Q1A);
+        TextView Q2 = (TextView) findViewById(R.id.Q2);
+        TextView Q2A = (TextView) findViewById(R.id.Q2A);
+        TextView Q3 = (TextView) findViewById(R.id.Q3);
+        TextView Q3A = (TextView) findViewById(R.id.Q3A);
+
+        
+
     }
 }
