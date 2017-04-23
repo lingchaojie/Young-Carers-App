33964e231a78a37695a6752ffecc990bd93b2003
lucywalsh <lucyhannahwalsh@gmail.com>
2017-04-23 12:51:48 +0100

    added pop up code and text

--- app/src/main/java/com/example/login_registernew/staffContact5.java
+++ app/src/main/java/com/example/login_registernew/staffContact5.java
@@ -2,6 +2,9 @@ package com.example.login_registernew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class staffContact5 extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class staffContact5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_contact5);
+
+        //get the resolution of the device screen and store it in variable dm
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        //initialize and setwidth and height variables of pop up window
+        int width = (dm.widthPixels);
+        int height = (dm.heightPixels);
+
+        //set the size of the pop up windows to 80% of the screen size
+        getWindow().setLayout((int) (width * 0.85), (int) (height * 0.5));
+
+        //set background to dim
+        WindowManager.LayoutParams windowManager = getWindow().getAttributes();
+        windowManager.dimAmount = 0.5f;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+
+        //initialise text views
+        TextView title = (TextView) findViewById(R.id.textView);
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView job = (TextView) findViewById(R.id.job);
+        TextView email = (TextView) findViewById(R.id.email);
+
+        //set text
+        title.setText("Contact");
+        name.setText("Jenna Robson");
+        job.setText("0-25 Carer Activity Facilitator");
+        email.setText("jenna@sunderlandcarers.co.uk");
     }
 }
